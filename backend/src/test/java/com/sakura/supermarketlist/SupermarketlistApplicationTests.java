80188eb58eac722807e8799a8d7c32a5008560e2
Caroline Santana <caroline.santana@ucsal.edu.br>
2026-04-11 18:44:11 -0300

    feat: configuração do banco de dados com o backend

---
+++ backend/src/test/java/com/sakura/supermarketlist/SupermarketlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakura.supermarketlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
