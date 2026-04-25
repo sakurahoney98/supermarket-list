7ea2a8ec7efb801f4813654a6cf0e3c2a860e65e
Caroline Santana <caroline.santana@ucsal.edu.br>
2026-04-25 09:12:44 -0300

    feat: inserção de entidades e repository closes #26

---
+++ backend/src/main/java/com/sakura/supermarketlist/compra/ItemCompraRepository.java
@@ -0,0 +1,7 @@
+package com.sakura.supermarketlist.compra;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemCompraRepository extends JpaRepository<ItemCompra, Long>{
+
+}
