1756e8f99f7b2954f7165b31052cd6d8f003af95
Caroline Santana <caroline.santana@ucsal.edu.br>
2026-05-28 15:42:04 -0300

    test: adição de testes closes #78

--- frontend/src/app/components/toast-message-error/toast-message-error.spec.ts
+++ frontend/src/app/components/toast-message-error/toast-message-error.spec.ts
@@ -1,22 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ToastMessageError } from './toast-message-error';
+import { ToastMessageErrorComponent } from './toast-message-error';
 
-describe('ToastMessageError', () => {
-  let component: ToastMessageError;
-  let fixture: ComponentFixture<ToastMessageError>;
+describe('ToastMessageErrorComponent', () => {
+  let component: ToastMessageErrorComponent;
+  let fixture: ComponentFixture<ToastMessageErrorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ToastMessageError],
+      imports: [ToastMessageErrorComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(ToastMessageError);
+    fixture = TestBed.createComponent(ToastMessageErrorComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve receber tituloErro via @Input',() => {
+    fixture.componentRef.setInput('tituloErro', 'Título do erro');
+    fixture.detectChanges();
+
+    const valorDOM: string = fixture.nativeElement.textContent ?? '';
+    expect(component.tituloErro).toBe('Título do erro');
+    expect(valorDOM).toContain('Título do erro');
+
+  });
+
+  it('deve receber mensagem via @Input',() => {
+    fixture.componentRef.setInput('mensagem', 'Mensagem do erro');
+    fixture.detectChanges();
+
+    const valorDOM: string = fixture.nativeElement.textContent ?? '';
+    expect(component.mensagem).toBe('Mensagem do erro');
+    expect(valorDOM).toContain('Mensagem do erro');
+
+  });
+
+  it('deve receber mostrarToast via @Input',() => {
+    fixture.componentRef.setInput('mostrarToast', true);
+    fixture.detectChanges();
+
+    expect(component.mostrarToast).toBe(true);
+    
+
   });
 });
