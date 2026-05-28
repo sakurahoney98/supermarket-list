c69592fa9c3a71565380ec27d8faa206f788690d
Caroline Santana <caroline.santana@ucsal.edu.br>
2026-05-28 15:42:12 -0300

    test: adição de testes closes #79

--- frontend/src/app/components/toast-message-sucess/toast-message-sucess.spec.ts
+++ frontend/src/app/components/toast-message-sucess/toast-message-sucess.spec.ts
@@ -1,22 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ToastMessageSucess } from './toast-message-sucess';
+import { ToastMessageSucessComponent } from './toast-message-sucess';
 
-describe('ToastMessageSucess', () => {
-  let component: ToastMessageSucess;
-  let fixture: ComponentFixture<ToastMessageSucess>;
+describe('ToastMessageSucessComponent', () => {
+  let component: ToastMessageSucessComponent;
+  let fixture: ComponentFixture<ToastMessageSucessComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ToastMessageSucess],
+      imports: [ToastMessageSucessComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(ToastMessageSucess);
+    fixture = TestBed.createComponent(ToastMessageSucessComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+   it('deve receber mensagem via @Input',() => {
+    fixture.componentRef.setInput('mensagem', 'Mensagem de sucesso');
+    fixture.detectChanges();
+
+    const valorDOM: string = fixture.nativeElement.textContent ?? '';
+    expect(component.mensagem).toBe('Mensagem de sucesso');
+    expect(valorDOM).toContain('Mensagem de sucesso');
+
+  });
+
+  it('deve receber mostrarToast via @Input',() => {
+    fixture.componentRef.setInput('mostrarToast', true);
+    fixture.detectChanges();
+
+    expect(component.mostrarToast).toBe(true);
+    
+
   });
 });
