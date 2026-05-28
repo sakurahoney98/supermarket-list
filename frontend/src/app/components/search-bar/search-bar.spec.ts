a2ad2762bd926e0512b6e9eaf216c457c3cec859
Caroline Santana <caroline.santana@ucsal.edu.br>
2026-05-28 15:39:00 -0300

    test: adição de testes closes #77

--- frontend/src/app/components/search-bar/search-bar.spec.ts
+++ frontend/src/app/components/search-bar/search-bar.spec.ts
@@ -1,17 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { vi } from 'vitest';
 
-import { SearchBar } from './search-bar';
+import { SearchBarComponent } from './search-bar';
 
-describe('SearchBar', () => {
-  let component: SearchBar;
-  let fixture: ComponentFixture<SearchBar>;
+describe('SearchBarComponent', () => {
+  let component: SearchBarComponent;
+  let fixture: ComponentFixture<SearchBarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SearchBar],
+      imports: [SearchBarComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(SearchBar);
+    fixture = TestBed.createComponent(SearchBarComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
@@ -19,4 +20,12 @@ describe('SearchBar', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('emitir evento busca ao chamar onSearch()', () => {
+    const emitSpy = vi.spyOn(component.busca, 'emit');
+
+    component.onSearch();
+
+    expect(emitSpy).toHaveBeenCalledOnce();
+  })
 });
