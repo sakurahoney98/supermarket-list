0a64976f8eed70d28ea7d38de2537d92ad2b7c93
Caroline Santana <caroline.santana@ucsal.edu.br>
2026-05-28 15:26:56 -0300

    test: adição de testes closes #74

--- frontend/src/app/components/list-action/list-action.spec.ts
+++ frontend/src/app/components/list-action/list-action.spec.ts
@@ -1,22 +1,65 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { vi } from 'vitest';
 
-import { ListAction } from './list-action';
+import { ListActionComponent } from './list-action';
 
-describe('ListAction', () => {
-  let component: ListAction;
-  let fixture: ComponentFixture<ListAction>;
+describe('ListActionComponent', () => {
+  let component: ListActionComponent;
+  let fixture: ComponentFixture<ListActionComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ListAction],
+      imports: [ListActionComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(ListAction);
+    fixture = TestBed.createComponent(ListActionComponent);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve receber quantidadeSelecionada via @Input', () => {
+    fixture.componentRef.setInput('quantidadeSelecionada', 5);
+
+    fixture.detectChanges();
+
+    const valorDOM: string = fixture.nativeElement.textContent ?? '';
+    expect(component.quantidadeSelecionada).toBe(5);
+    expect(valorDOM).toContain('5')
+  });
+
+  it('deve emitir o evento excluir ao chamar onExcluirClick()', () => {
+    const emitSpy = vi.spyOn(component.excluir, 'emit');
+
+    component.onExcluirClick()
+
+    expect(emitSpy).toHaveBeenCalledOnce();
+  });
+
+  it('deve emitir true em selecionarTudo quando todosItensSelecionados for false', () => {
+    component.todosItensSelecionados = false;
+  const emitSpy = vi.spyOn(component.selecionarTudo, 'emit');
+
+  component.toggleBotaoSelecionarTudoAtivado();
+
+  expect(emitSpy).toHaveBeenCalledWith(true);
+  expect(emitSpy).toHaveBeenCalledOnce();
+
+  });
+
+  it('deve emitir false em selecionarTudo quando todosItensSelecionados for true', () => {
+   component.todosItensSelecionados = true;
+  const emitSpy = vi.spyOn(component.selecionarTudo, 'emit');
+
+  component.toggleBotaoSelecionarTudoAtivado();
+
+  expect(emitSpy).toHaveBeenCalledWith(false);
+  expect(emitSpy).toHaveBeenCalledOnce();
+
+  });
+
+
 });
